196d7aac8f22001ebd37f1163a159e6cf629cca6
Alirezabg <alirezabg@gmail.com>
2025-05-03 20:00:51 +0100

    change github action tf plan to use pipeline

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "cyf-cloud"
     key            = "envs/prod/terraform.tfstate"
-    region         = "eu-west-1"         # ← Ireland
+    region         = "eu-west-1"         #  Ireland
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
