339b163e3a36159ff71f7614140b13c73ad7e066
Alirezabg <alirezabg@gmail.com>
2025-05-03 20:20:29 +0100

    remove resources

--- main.tf
+++ main.tf
@@ -9,9 +9,6 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
-provider "aws" {
-  region  = "us-west-2"
-}
 
 resource "aws_s3_bucket_versioning" "app_bucket_versioning" {
   bucket = aws_s3_bucket.app_bucket.id
