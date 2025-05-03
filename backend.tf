13a59a843b3cc27dd574423f4209cbd101df28dd
Alirezabg <alirezabg@gmail.com>
2025-05-03 20:39:59 +0100

    rm unused variable

--- backend.tf
+++ backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket         = "cyf-cloud"
     key            = "envs/prod/terraform.tfstate"
     region         = "eu-west-1"         #  Ireland
-    dynamodb_table = "terraform-locks"
     encrypt        = true
   }
 }
