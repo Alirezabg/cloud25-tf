505327e026699bf5514eb4010afdbdb514f8656f
Alirezabg <alirezabg@gmail.com>
2025-05-03 20:38:10 +0100

    rm unused variable

--- main.tf
+++ main.tf
@@ -8,12 +8,3 @@ terraform {
 
   required_version = ">= 1.2.0"
 }
-
-
-resource "aws_s3_bucket_versioning" "app_bucket_versioning" {
-  bucket = aws_s3_bucket.app_bucket.id
-  
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
