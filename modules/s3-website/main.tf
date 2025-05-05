7b4cf5cd66ca53ca4bf6cf4da9c4c0c87010cd9d
Alirezabg <alirezabg@gmail.com>
2025-05-05 12:28:27 +0100

    create s3 for website -- fix typo

--- modules/s3-website/main.tf
+++ modules/s3-website/main.tf
@@ -29,9 +29,9 @@ data "aws_iam_policy_document" "public_read" {
         effect ="Allow"
         principals {
             type = "AWS"
-            indetifiers =["*"]
+            identifiers  =["*"]
         }
-        action = ["s3:GetObject"]
+        actions = ["s3:GetObject"]
         resources = ["${aws_s3_bucket.this.arn}/*"]
     }
 }
