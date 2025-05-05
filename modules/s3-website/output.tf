d600991aada4d259ba4366dc8e8b7c98f2933723
Alirezabg <alirezabg@gmail.com>
2025-05-05 18:53:08 +0100

    fix pipeline

--- modules/s3-website/output.tf
+++ modules/s3-website/output.tf
@@ -1,4 +1,3 @@
 output "website_endpoint" {
-    description = "S3 website endpoint"
-    value = aws_s3_bucket.this.website_endpoint
+  value = aws_s3_bucket_website_configuration.this.website_endpoint
 }
