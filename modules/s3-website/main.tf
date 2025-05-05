59c19e21d4a4280c10433682e1d51bc27b6269da
Alirezabg <alirezabg@gmail.com>
2025-05-05 14:27:36 +0100

    remove depricated part

--- modules/s3-website/main.tf
+++ modules/s3-website/main.tf
@@ -4,11 +4,12 @@ provider "aws"{
 
 resource "aws_s3_bucket" "this"{
     bucket = var.bucket_name
-    acl = "public-read"
-    website {
-        index_document = "index.html"
-        error_document = "index.html"
+    index_document {
+            suffix = "index.html"
+        }
 
+    error_document {
+        key = "index.html"
     }
     tags = {
         Name = var.bucket_name
