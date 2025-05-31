3bd42681d6bbd899801e7cc60193e59a8ba9f8de
Alirezabg <alirezabg@gmail.com>
2025-05-31 07:03:37 +0100

    change t2 rds to t3

--- rds.tf
+++ rds.tf
@@ -4,7 +4,7 @@ module "rds_db" {
   identifier         = "my-app-db"
   engine             = "mysql"
   engine_version     = "8.0.34"
-  instance_class     = "db.t2.micro"
+  instance_class     = "db.t3.micro"
   allocated_storage  = 20  
   vpc_id            = module.vpc.vpc_id
   subnet_ids         = module.vpc.subnet_ids
